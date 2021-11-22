405c53c664322b16cb628e04598bbaa3a4ad4cc8
Abigail Vickers <ahopevickers@gmail.com>
2021-11-22 13:04:30 -0500

    Changed argument name for select_input() from x to choices and began building function select_options().

--- R/select_input.R
+++ R/select_input.R
@@ -2,19 +2,19 @@
 #'
 #' @param inputId A shiny input ID
 #' @param label A label for the input
-#' @param x A vector of choices for the input
+#' @param choices A vector of choices for the input
 #' @param type The type of input, select for selectInput, multiple for selectInput multiple = TRUE, and checkbox for checkboxGroupInput
 #' @return A shiny widget that allows user to select from a list of choices
 #' @examples
 #' select_input("color", "Color By:", c("None", "Temp", "Day/Night"), "select")
 
-select_input <- function(inputId, label, x, type) {
+select_input <- function(inputId, label, choices, type) {
   if (type == "select") {
-    shiny::selectInput(inputId, label, choices = x)
+    shiny::selectInput(inputId, label, choices)
   } else if (type == "multiple") {
-    shiny::selectInput(inputId, label, choices = x, multiple = TRUE)
+    shiny::selectInput(inputId, label, choices, multiple = TRUE)
   } else if (type == "checkbox") {
-    shiny::checkboxGroupInput(inputId, label, choices = x)
+    shiny::checkboxGroupInput(inputId, label, choices)
   } else {
     stop("Invalid type, must choose from select, multiple, or checkbox")
   }
---
+++ R/select_options.R
@@ -0,0 +1,4 @@
+select_options <- function(df, col, all = NULL, none = NULL) {
+  # unique characters of variables, all = TRUE adds "All", none = TRUE adds "None"
+  # output as vector of options as characters
+}
