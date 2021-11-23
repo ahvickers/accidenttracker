b94700407b2822f5111d70f61f6d565391569057
Abigail Vickers <ahopevickers@gmail.com>
2021-11-23 14:49:46 -0500

    Added function to create a vector with the list of options from a column and added basic documentation

--- R/select_options.R
+++ R/select_options.R
@@ -1,4 +1,19 @@
-select_options <- function(df, col, all = NULL, none = NULL) {
-  # unique characters of variables, all = TRUE adds "All", none = TRUE adds "None"
-  # output as vector of options as characters
+#' Create a character vector of unique values from a column
+#'
+#' @param df A dataframe
+#' @param col The name of a column from the dataframe
+#' @param all Default is FALSE, when TRUE will add "All" to the vector
+#' @param none Default is FALSE, when TRUE will add "None" to the vector
+#' @return A character vector
+
+select_options <- function(df, col, all = FALSE, none = FALSE) {
+  column <- dplyr::pull(df, var = {{col}})
+  if (all == TRUE) {
+    x <- c("All", sort(unique(column)))
+  } else if (none == TRUE) {
+    x <- c("None", sort(unique(column)))
+  } else {
+    x <- c(sort(unique(column)))
+  }
+  x
 }
