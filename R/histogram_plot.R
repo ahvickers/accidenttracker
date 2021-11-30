a6204dc1a1423d96856900c67a27d3ad2f0b8e48
Abigail Vickers <ahopevickers@gmail.com>
2021-11-30 17:28:43 -0500

    Added basic documentation to histogram_plot() and updated function to match changes made in app to allow for change in number of bins.

--- R/histogram_plot.R
+++ R/histogram_plot.R
@@ -1,11 +1,17 @@
+#' Switch between histogram or bar plot based on type of data
+#'
+#' @param x A variable to plot
+#' @param y The number of bins
+#' @return A histogram plot if continuous or a bar plot if categorical
+#' @export
 
 histogram_plot <- function(x, y){
-  if(x == "state" | x == "day.night" | x == "wind.dir" | x == "side"){
+  if(x == "state" | x == "day.night" | x == "wind.dir" | x == "side" | x == "month" | x == "hour"){
     ggplot(us_accidents, aes_string(x)) +
       geom_bar(width=1)
   }
   else{
     ggplot(us_accidents, aes_string(x)) +
-      geom_histogram(binwidth=y)
+      geom_histogram(bins = y)
   }
 }
