bd3830d38c4951d406f1825594d79f9f55155660
bestrem <89321623+bestrem@users.noreply.github.com>
2021-11-30 14:21:57 -0500

    added two options to plot

--- R/histogram_plot.R
+++ R/histogram_plot.R
@@ -1,6 +1,6 @@
 
 histogram_plot <- function(x, y){
-  if(x == "state" | x == "day.night"){
+  if(x == "state" | x == "day.night" | x == "wind.dir" | x == "side"){
     ggplot(us_accidents, aes_string(x)) +
       geom_bar(width=1)
   }
